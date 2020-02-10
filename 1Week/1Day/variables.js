68adf115ecee57a7a97e27bc0a59296d18c944d6
gerard-cancino <gerard.cancino@gmail.com>
2020-02-10 13:42:44 -0500

    1Week 1Day Helloworld Variables

---
+++ 1Week/1Day/variables.js
@@ -0,0 +1,11 @@
+var name = 'Nikoloi';
+
+// Use this instead of var
+// let and const were a part of es6
+// the function almost the same as var, but they have better variable scoping
+// 
+let name2 = 'Ramel';
+// Let + Final = No Reassignment
+const name3 = 'Alec';
+
+console.log(`${name} + ${name2} + ${name3}`);
